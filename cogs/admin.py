3616a2dec87848aee40760f23d159f37c8dd9de6
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-02-02 23:19:29 +0630

    Documented and added form publishing

--- cogs/admin.py
+++ cogs/admin.py
@@ -4,13 +4,26 @@ import json
 from discord.ext import commands, tasks
 
 class Admin(commands.Cog):
+	
+	'''Administration and maintenance based commands and functions
+	
+	- Reload / load / unload
+	- Pushing updates and threads to guild owners
+	- Restarting / Shutting down the bot'''
 
 	def __init__(self, client):
 		self.client = client
 		self.color = 0x9494FF
+		self.change_status.start()
+
+	'''This is a custom ID check designed to disapprove any 
+	alien acceses'''
 
 	def botAdminCheck(ctx):
 		return ctx.message.author.id == 368671236370464769 # change this number to your ID
+
+	'''Pushing and publishing updates and threads to 
+	guild owners, this part can be mostly ignored'''
 
 	@commands.command(name = "restart subslist", aliases=['rstl'])
 	@commands.guild_only()
@@ -32,24 +45,20 @@ class Admin(commands.Cog):
 		with open('cogs/subscriptions.json', 'r') as f:
 			people_subscribed = json.load(f)
 		sent = []
-		embed = discord.Embed(title = "__# Notice__", description= "Hey there, recently we've recieved a lot of feedbacks on the poor lobbying system and we've worked on it now to work a tad lot better!",color = self.color)
-		embed.add_field(name= '\nWhats new?:', value = f"```- Better ready up process\nYou can now ready up for lobbies by reacting to the message with the reaction it says.\n- Game per channel\nPictionary games are now restricted per channel to avoid blockage, although it's allowed and possible to play multiple games on different channels.```", inline = False)
-		embed.add_field(name= '\nTip:', value = f'`To unsubscribe from this mailing list as a guild owner, please react to this with 🙅‍♂️`', inline = True)
-		embed.add_field(name= '\nPictionary code base:', value = f'https://github.com/Ricky-MY/The-Pictionary-Bot', inline = True)
-		embed.add_field(name= '\nPrefix Commands:', value = f'`~prefix` (Shows Current Prefix)\n`~prefix change <new_prefix>`(Changes Current Prefix)', inline = False)
-		embed.add_field(name= '\u200b',value= f'[Join our support server and have a say in what we do next!](https://discord.gg/UmnzdPgn6g)', inline = False)
-		embed.set_footer(text='Have a wonderful time playing pictionary!')
+		embed = discord.Embed(title = "__# Notice__", description= "description")
 		for key in people_subscribed.keys():
 			if int(key) in [i.id for i in guilds]:
 				if people_subscribed[key] not in sent:
 					sent.append(people_subscribed[key])
 					member = self.client.get_guild(int(key)).get_member(people_subscribed[key])
 					message = await member.send(embed = embed)
-					message.add_reaction('🙅‍♂️')
+					await message.add_reaction('🙅‍♂️')
 			elif int(key) not in [i.id for i in guilds]:
 				people_subscribed.pop(key)
 		with open('cogs/subscriptions.json', 'w') as f:
 			json.dump(people_subscribed, f)
+
+	'''Utility commands to provide administrator access on the bot'''
 
 	# Guilds Checker
 	@commands.command(name="guilds")
@@ -72,6 +81,7 @@ class Admin(commands.Cog):
 							   color=self.color)
 		await ctx.send(embed=embed)
 
+	# Unload command
 	@commands.command(name="unload", aliases=['ul'])
 	@commands.check(botAdminCheck)
 	async def unload_cogs(self, ctx, extension):
@@ -81,6 +91,7 @@ class Admin(commands.Cog):
 								color=self.color)
 		await ctx.send(embed=embed)
 
+	# Reload command
 	@commands.command(name="reload", aliases=['rl'])
 	@commands.check(botAdminCheck)
 	async def reload_cogs(self, ctx, extension):
@@ -88,7 +99,8 @@ class Admin(commands.Cog):
 		embed = discord.Embed(title='Success!', description=f'{extension} is reloaded.',
 									color=self.color)
 		await ctx.send(embed=embed)
-			
+	
+	# Load and reload all cogs
 	@commands.command(name="restart", aliases=['rst', 'sync'])
 	@commands.check(botAdminCheck)
 	async def restart(self, ctx):
@@ -98,6 +110,9 @@ class Admin(commands.Cog):
 				self.client.load_extension(f'cogs.{filename[:-3]}')
 		await ctx.send(embed=discord.Embed(title='Success!', description=f'Bot has restarted', color=self.color))
 
+	'''This is purely preferential, this piece of code or loop is used 
+	to update the bot status on how many servers it is currently invited into.'''
+	
 	@tasks.loop(hours = 2)
 	async def change_status(self):
 		try:
