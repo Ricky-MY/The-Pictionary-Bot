47041e247b8db138653b1c4ace80313818950be9
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-02-04 15:56:28 +0630

    updated bot constructor

--- main.py
+++ main.py
@@ -8,39 +8,46 @@ intents.reactions = True
 intents.members = True
 intents.reactions = True
 
-def get_prefix(client, message):
+main_directory = 'src'
+
+def get_prefix(bot, message):
 	with open('prefixes.json', 'r') as f:
 		prefixes = json.load(f)
 	try:
-		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(client, message)
+		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
 	except KeyError:
 		with open('prefixes.json', 'r') as f:
 			prefixes = json.load(f)
 		prefixes[str(message.guild.id)] = "~"
 		with open('prefixes.json', 'w') as f:
 			json.dump(prefixes, f, indent=4)
-		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(client, message)
+		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
 	except AttributeError:
 		pass
 
-client = commands.Bot(command_prefix=get_prefix, intents = intents)
-client.remove_command('help')
+bot = commands.Bot(command_prefix=get_prefix, status=discord.Status.online, activity=discord.Game(f'~help'), intents = intents)
+bot.remove_command('help')
 
-# Shut Down Command
-@client.command(name="shutdown")
+@bot.command(name="shutdown")
 @commands.guild_only()
 async def shutdown(ctx):
 	await ctx.send('Bot has been shut down.')
-	await client.logout()
+	await bot.logout()
 
-@client.event
+@bot.event
 async def on_ready():
 	print("________________________________________________________________________________________________________________________\nPictionary is on standby\n________________________________________________________________________________________________________________________")
-	await client.change_presence(status=discord.Status.online, activity=discord.Game(f'~help | {len(client.guilds)} guilds'))
 
-for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
+to_reload = []
+for filename in os.listdir(f'./{main_directory}'):
+	if filename.endswith('.py') and not filename.startswith('_'):
+		bot.load_extension(f'{main_directory}.{filename[:-3]}')
+	elif not filename.endswith('.py') and not filename.endswith('.json') and not filename.startswith('_'):
+		to_reload.append(filename)
+for file in to_reload:
+	for filename in os.listdir(f'./{main_directory}/{file}'):
+		if filename.endswith('.py') and not filename.startswith('_'):
+			bot.load_extension(f'{main_directory}.{file}.{filename[:-3]}')
 
 THIS_MA_TOKEN_HEHE = open('THIS_MA_TOKEN_HEHE.txt', 'r').readline()
-client.run(THIS_MA_TOKEN_HEHE)
+bot.run(THIS_MA_TOKEN_HEHE)
