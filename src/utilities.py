b6debc156ee9e6c2e8314eaa132d8af762d9eedd
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-02-05 18:40:06 +0630

    New utility cog for feedback and misc commands

---
+++ src/utilities.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands, menus
+
+class Util(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.color = 0x87ceeb
+
+    @commands.command(name= "feedback", aliases = ['fb', 'report'])
+    async def feedback(self, ctx, *, feedback):
+        guild = discord.utils.find(
+                lambda g: g.id == 793047973751554088, self.bot.guilds)
+        channel = guild.get_channel(807217915006287880)
+        to_deliver = embed=discord.Embed(title="Feedback", description=f"Feedback:\n```{feedback}```\nGuild: {ctx.guild.name}\nOwner: <@{ctx.guild.owner_id}>", color = self.color)
+        to_deliver.set_footer(text=f'By {ctx.author.name}', icon_url=ctx.author.avatar_url)
+        await channel.send(embed = to_deliver)
+        await ctx.send(embed=discord.Embed(title="Success!", description=f"Thank you so much for your feedback. We will notify the developer right away.\nFeedback:```{feedback}```", color = self.color))
+
+def setup(bot):
+    bot.add_cog(Util(bot))
+    print('Util.cog is loaded')
