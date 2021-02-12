54bba2f5ff759993941f51a3a9121b9813184189
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-02-13 01:22:41 +0630

    rename

--- webpage/canvas.js
+++
@@ -1,8 +0,0 @@
-let canvas = document.querySelector('#canvas');
-
-// Resizing of the canvas window
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Context is used to change things on the canvas
-let ctx = canvas.getContext('2d');
